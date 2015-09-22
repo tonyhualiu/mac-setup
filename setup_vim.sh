3f597d9c1cefe780bea757c4b526d16c861d4caf
Tony Liu <tonyhualiu1912@gmail.com>
2015-09-22 00:16:26 -0700

    add vim setup file
    Just to download all the plugins

---
+++ setup_vim.sh
@@ -0,0 +1,9 @@
+VIM_BUNDLE_PATH="~/.vim/bundle"
+git clone https://github.com/bling/vim-airline.git $VIM_BUNDLE_PATH
+git clone https://github.com/ervandew/supertab.git $VIM_BUNDLE_PATH
+git clone https://github.com/kien/ctrlp.vim.git $VIM_BUNDLE_PATH
+git clone https://github.com/scrooloose/nerdtree.git $VIM_BUNDLE_PATH
+git clone https://github.com/scrooloose/syntastic.git $VIM_BUNDLE_PATH
+git clone https://github.com/SirVer/ultisnips $VIM_BUNDLE_PATH
+git clone https://github.com/bronson/vim-trailing-whitespace.git $VIM_BUNDLE_PATH
+
